addefda05677460d97dadc7217052ba627f7f424
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-05 21:07:30 +0700

    Fix login on loginnew.blade.php

--- AplikasiIndogrosir/resources/views/auth/loginnew.blade.php
+++ AplikasiIndogrosir/resources/views/auth/loginnew.blade.php
@@ -63,14 +63,15 @@
 
                     <h4 class="text-dark mb-6 text-center">Sign in for free</h4>
 
-                    <form action="/index.html">
+                    <form method="POST" action="{{ route('login') }}">
+                      @csrf
                       <div class="row">
                         <div class="form-group col-md-12 mb-4">
-                          <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp"
+                          <input type="email" class="form-control input-lg" name="email" aria-describedby="emailHelp"
                             placeholder="email">
                         </div>
                         <div class="form-group col-md-12 ">
-                          <input type="password" class="form-control input-lg" id="password" placeholder="Password">
+                          <input type="password" class="form-control input-lg" name="password" placeholder="Password">
                         </div>
                         <div class="col-md-12">
 
