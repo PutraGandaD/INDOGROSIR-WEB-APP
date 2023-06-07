5e89693a008afc44c9f191c44bf63a1ee9600e52
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-08 00:21:28 +0700

    Added Create/Store to Brand

---
+++ AplikasiIndogrosir/resources/views/brand/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.main')
+
+@section('title', 'Tambah Brand')
+@section('subtitle', 'Brand')
+@section('content')
+
+<div class="row">
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Brand</h4>
+            <p class="card-description">
+              Formulir tambah Brand
+            </p>
+            <form class="forms-sample" action="{{ route('brand.store')}}" method="post" enctype="multipart/form-data">
+              @csrf
+              <div class="form-group">
+                <label for="nama_brand">Nama Brand</label>
+                <input type="text" class="form-control" name="nama_brand" placeholder="Nama Brand" value="{{ old('nama_brand') }}">
+                @error('nama_brand')
+                    <span class="text-danger">{{ $message }} </span>
+                @enderror
+              </div>
+              <div class="form-group">
+                <label for="logo_brand">Logo Brand</label>
+                <input type="file" class="form-control" name="logo_brand" placeholder="logo_brand" accept="image/*" capture="camera">
+                @error('logo_brand')
+                   <span class="text-danger">{{$message}}</span>
+                @enderror
+              </div>
+              <button type="submit" class="btn btn-primary me-2 mt-3">Simpan</button>
+              <a href="{{ route('brand.index')}}" class="btn btn-light mt-3">Batal</button>
+            </form>
+          </div>
+        </div>
+      </div>
+</div>
+
+@endsection
+
