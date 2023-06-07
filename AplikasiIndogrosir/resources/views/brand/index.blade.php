ae1bbcb7d9643ebe4c1a9422316d7e3e2ed97760
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-08 00:21:28 +0700

    Added Create/Store to Brand

--- AplikasiIndogrosir/resources/views/brand/index.blade.php
+++ AplikasiIndogrosir/resources/views/brand/index.blade.php
@@ -5,7 +5,41 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <h1>halaman Brand</h1>
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+      <div class="card">
+        <div class="card-body">
+          @if (Session::get('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+            </div>
+          @endif
+          <h4 class="card-title">Brand</h4>
+          <a href="{{ route('brand.create')}}" class="btn btn-primary">Tambah</a>
+          <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <th>Nama Brand</th>
+                    <th>Logo Brand</th>
+                    <th>Created At</th>
+                </thead>
+                @foreach ($brand as $item )
+                <tbody>
+                    <td>{{$item -> nama_brand}}</td>
+                    <td> <img width="50"
+                        src="{{ $item->logo_brand ? asset('storage/' . $item->logo_brand) : asset('images/faces/face5.jpg') }}">
+                    </td>
+                    <td>{{$item -> created_at}}</td>
+                </tbody>
+                @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
+
+    <table class="table table-hover">
+
+    </table>
 @endsection
