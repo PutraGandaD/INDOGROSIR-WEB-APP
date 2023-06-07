aac124b2c9f7f8bd697182a7bf9f81c2b9ea25d4
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-08 01:23:18 +0700

    Add nama_produk to Produk migration

--- AplikasiIndogrosir/resources/views/produk/index.blade.php
+++ AplikasiIndogrosir/resources/views/produk/index.blade.php
@@ -1,11 +1,43 @@
 @extends('layout.main')
 @section('pagetitle', 'Produk')
-@section('hoverproduk','active')
-
+@section('hoverbrand','active')
 
 @section('content')
 
-<div class="container-fluid">
-    <h1>halaman Produk</h1>
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+      <div class="card">
+        <div class="card-body">
+          @if (Session::get('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+            </div>
+          @endif
+          <h4 class="card-title">Produk</h4>
+          <a href="{{ route('produk.create')}}" class="btn btn-primary">Tambah</a>
+          <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <th>Nama Produk</th>
+                    <th>Brand Produk</th>
+                    <th>Foto Produk</th>
+                    <th>Kontainer</th>
+                    <th>Harga</th>
+                </thead>
+                @foreach ($kontainerBarang as $item )
+                <tbody>
+                    <td>{{$item -> nama_produk}}</td>
+                    <td>{{$item -> created_at}}</td>
+                </tbody>
+                @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
+
+    <table class="table table-hover">
+
+    </table>
 @endsection
