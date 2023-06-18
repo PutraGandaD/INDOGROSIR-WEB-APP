cdabfb9267287848782e11e7c17bbbd46b1f595b
zaraazzar <azzar.arza18@gmail.com>
2023-06-19 05:40:25 +0700

    add register & logout API

--- AplikasiIndogrosir/app/Http/Controllers/API/AuthController.php
+++ AplikasiIndogrosir/app/Http/Controllers/API/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends BaseController
 {
@@ -26,5 +28,33 @@ class AuthController extends BaseController
     }
 
     //register
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        $validated['password'] = Hash::make($request->password);
+        User::create($validated);
+        $user = User::where('email', $request->email)->first();
 
+
+            $token = $user->createToken('kelompok friends')->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'token' => $token
+            ], 200);
+    }
+
+    //logout
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses berhasil logout',
+        ],);
+    }
 }
